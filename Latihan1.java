80be9cc9928b92fab4f6c3e71d8d6836c0da82d0
UlfahPratiwi <ulfahpratiwi.bkt@gmail.com>
2022-11-04 21:00:02 +0700

    Create Latihan1.java

---
+++ Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan1 {
+    public static void main(String[] args){
+        
+        //arguments = java Hello world that is all
+        
+        
+        int i;
+        for(i=1;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
